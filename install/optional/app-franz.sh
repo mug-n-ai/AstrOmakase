df5a8a67c2151f44241468d75f8ba1e3218c7bfc
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-07-13 12:55:29 +0200

    franz fixed and tested

--- install/optional/app-franz.sh
+++ install/optional/app-franz.sh
@@ -1,8 +1,50 @@
-sudo apt install -y libx11-dev libxext-dev libxss-dev libxkbfile-dev
+# Funzione per stampare messaggi di errore
+print_error() {
+    echo "[ERROR] $1" >&2
+}
 
-cd /tmp
-wget -O franz.deb 'https://github.com/meetfranz/franz/releases/download/v5.10.0/franz_5.10.0_amd64.deb'
-sudo apt install -y ./franz.deb
-rm slack.deb
-cd -
+# Funzione per stampare messaggi di successo
+print_success() {
+    echo "[SUCCESS] $1"
+}
+
+echo "Installing dependencies..."
+sudo apt install -y libx11-dev libxext-dev libxss-dev libxkbfile-dev
+if [ $? -ne 0 ]; then
+    print_error "Failed to install dependencies. Exiting."
+    exit 1
+fi
+
+echo "Fetching the latest Franz release download link..."
+LATEST_FRANZ_URL=$(curl -s https://api.github.com/repos/meetfranz/franz/releases/latest | grep "browser_download_url.*amd64.deb" | cut -d '"' -f 4)
+if [ -z "$LATEST_FRANZ_URL" ]; then
+    echo "Unable to fetch the latest Franz release download link. Exiting."
+    exit 1
+fi
+
+echo "Downloading Franz..."
+wget -O /tmp/franz.deb "$LATEST_FRANZ_URL"
+if [ $? -ne 0 ]; then
+    print_error "Failed to download Franz. Exiting."
+    exit 1
+fi
+
+echo "Installing Franz..."
+sudo apt install -y /tmp/franz.deb
+if [ $? -ne 0 ]; then
+    print_error "Failed to install Franz. Exiting."
+    exit 1
+fi
+
+# Rimuovere il file .deb scaricato
+echo "Cleaning up..."
+rm /tmp/franz.deb
+if [ $? -ne 0 ]; then
+    print_error "Failed to remove temporary files."
+else
+    print_success "Removed temporary files."
+fi
+
+print_success "Franz installation script completed successfully."
+
 
