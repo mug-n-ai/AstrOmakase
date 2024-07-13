7d8295a95ac7e7ec36de7c09c87eea9fecfc55f1
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-07-13 13:09:23 +0200

    scrcpy updated and tested

--- install/optional/app-scrcpy.sh
+++ install/optional/app-scrcpy.sh
@@ -1,12 +1,56 @@
+
+# Destination directory for the AppImage
+APP_DIR="$HOME/Applications"
+mkdir -p $APP_DIR
+
+# Function to print error messages
+print_error() {
+    echo "[ERROR] $1" >&2
+}
+
+# Function to print success messages
+print_success() {
+    echo "[SUCCESS] $1"
+}
+
+echo "Installing dependencies..."
 sudo apt install -y ffmpeg libsdl2-2.0-0 adb wget \
                  gcc git pkg-config meson ninja-build libsdl2-dev \
                  libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev \
                  libswresample-dev libusb-1.0-0 libusb-1.0-0-dev
+if [ $? -ne 0 ]; then
+    print_error "Failed to install dependencies. Exiting."
+    exit 1
+fi
+print_success "Dependencies installed successfully."
 
-cd ~/Applications
+echo "Checking if scrcpy directory exists and is not empty..."
+if [ -d "$APP_DIR/scrcpy" ] && [ "$(ls -A $APP_DIR/scrcpy)" ]; then
+    echo "Directory exists and is not empty. Deleting directory..."
+    rm -rf "$APP_DIR/scrcpy"
+    if [ $? -ne 0 ]; then
+        print_error "Failed to delete existing scrcpy directory. Exiting."
+        exit 1
+    fi
+    print_success "Existing scrcpy directory deleted successfully."
+fi
 
-git clone https://github.com/Genymobile/scrcpy
-cd scrcpy
-bash ./install_release.sh
+echo "Cloning scrcpy repository..."
+git clone https://github.com/Genymobile/scrcpy $APP_DIR/scrcpy
+if [ $? -ne 0 ]; then
+    print_error "Failed to clone scrcpy repository. Exiting."
+    exit 1
+fi
+print_success "scrcpy repository cloned successfully."
+
+
+echo "Installing scrcpy..."
+cd $APP_DIR/scrcpy
+source $APP_DIR/scrcpy/install_release.sh
+if [ $? -ne 0 ]; then
+    print_error "Failed to install scrcpy. Exiting."
+    exit 1
+fi
+print_success "scrcpy installed successfully."
 
 cd -
