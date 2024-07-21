086111862b63937b56a386f9490ca3e3b5166c03
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-07-21 16:38:02 +0200

    boot launcher fixed

--- boot.sh
+++ boot.sh
@@ -4,19 +4,18 @@
 
 set -e
 
-ascii_art='AstrOmakase'
+ascii_art="AstrOmakase"
 
 echo -e "$ascii_art"
 echo "=> AstrOmakase is for fresh Ubuntu 24.04 installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
-
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
 echo "Cloning stable AstrOmakase..."
 rm -rf ~/.local/share/astromakase
-git clone -b stable https://github.com/basecamp/omakub.git ~/.local/share/astromakase >/dev/null
+git clone -b stable https://github.com/LorenzoMugnai/AstrOmakase.git ~/.local/share/astromakase >/dev/null
 
 echo "Installation starting..."
 source ~/.local/share/astromakase/install.sh
