9afdfcf62d59489bb4f6a5b879ac6eaaed07897c
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-07-21 16:23:48 +0200

    updates, added brave

---
+++ install/optional/app-brave.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../../common_functions.sh"
+
+echo "Checking if Brave is installed..."
+if command_exists brave-browser; then
+    print_success "Brave is already installed. Skipping."
+else
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update -y
+  sudo apt install -y brave-browser
+
+  if [ $? -ne 0 ]; then
+      print_error "Failed to install Brave. Exiting."
+      exit 1
+  fi
+fi
---
+++ install/unistall_uneeded.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OMAKUB_DIR="$HOME/.local/share/omakub"
+
+
+# Uninstall un-needed Omakub software
+echo "Removing un-needed tools"
+TO_REMOVE_APP=("1password" "audacity" "ollama" "pinta" "signal" "spotify" "steam" "web" "rubymine")
+for app in "${TO_REMOVE_APP[@]}"; do
+    echo "Uninstalling ${app}..."
+    if ! source "$OMAKUB_DIR/uninstall/app-${app}.sh"; then
+        echo "Failed to uninstall ${app}. It might not be installed."
+    fi
+done
