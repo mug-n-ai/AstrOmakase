e2eac574fd8e6a5011f103dfeba429e4b03b2f87
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-07-31 18:44:14 +0200

    using cleaned functions for required installations too

--- required.sh
+++ required.sh
@@ -9,22 +9,12 @@ if ! command_exists gum; then
     # Ensure snapd is installed
     if ! command_exists snap; then
         echo "snap is not installed. Installing snap..."
-        sudo apt update
-        sudo apt install -y snapd
-        if [ $? -ne 0 ]; then
-            print_error "Failed to install snap. Exiting."
-            exit 1
-        fi
-        print_success "snap installed successfully."
+        apt_install snapd
     fi
 
     # Install gum using snap
-    sudo snap install gum
-    if [ $? -ne 0 ]; then
-        print_error "Failed to install gum. Exiting."
-        exit 1
-    fi
-    print_success "gum installed successfully."
+    snap_install gum
+
 else
     print_success "gum is already installed."
 fi
@@ -32,12 +22,7 @@ fi
 echo "Checking for Flatpak installation..."
 if ! command_exists flatpak; then
     echo "Flatpak is not installed. Installing Flatpak..."
-    sudo apt install -y flatpak
-    if [ $? -ne 0 ]; then
-        print_error "Failed to install Flatpak. Exiting."
-        exit 1
-    fi
-    print_success "Flatpak installed successfully."
+    apt_install flatpak
 else
     print_success "Flatpak is already installed."
 fi
