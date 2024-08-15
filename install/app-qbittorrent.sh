7c81674dbb991d3512b4bca2c8d42eb2c396c905
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-08-15 12:05:26 -0700

    qbittorrent fixed

--- install/app-qbittorrent.sh
+++ install/app-qbittorrent.sh
@@ -3,8 +3,12 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../common_functions.sh"
 
-echo "Installing qBittorrent..."
-sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
-sudo apt-get update
+if command_exists qbittorrent; then
+    print_success "qBittorrent is already installed. Exiting script."
+    else
+    echo "Installing qBittorrent..."
+    sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+    sudo apt-get update
 
-apt_install qbittorrent
+    apt_install qbittorrent
+fi
