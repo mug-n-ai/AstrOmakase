05f1301d62619fb44ecb460a037ac87719f0da6c
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-08-15 11:36:04 -0700

    bin updated

--- settings/set-launcher.sh
+++ settings/set-launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# Creazione del file AstrOmakase.desktop
 cat <<EOF >~/.local/share/applications/AstrOmakase.desktop
 [Desktop Entry]
 Version=1.0
@@ -15,16 +15,18 @@ MimeType=text/html;text/xml;application/xhtml_xml;
 StartupNotify=true
 EOF
 
+# Aggiungi i permessi di esecuzione al file astromakase
+sudo chmod +x $HOME/.local/share/astromakase/bin/astromakase
 
 # Aggiunta del percorso al file .bashrc solo se non esiste già
-if ! grep -q 'export PATH="$HOME/.local/share/astromakub/bin:$PATH"' "$HOME/.bashrc"; then
-    echo 'export PATH="$HOME/.local/share/astromakub/bin:$PATH"' >> "$HOME/.bashrc"
+if ! grep -q 'export PATH="$HOME/.local/share/astromakase/bin:$PATH"' "$HOME/.bashrc"; then
+    echo 'export PATH="$HOME/.local/share/astromakase/bin:$PATH"' >> "$HOME/.bashrc"
     echo "PATH added to .bashrc"
 else
     echo "PATH already exists in .bashrc"
 fi
 
 # Rendi effettivo il cambiamento nella sessione corrente
-export PATH="$HOME/.local/share/astromakub/bin:$PATH"
+export PATH="$HOME/.local/share/astromakase/bin:$PATH"
 
 echo "Setup complete!"
