90a351fe84f748cae71f735298eea74789a739f9
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-08-15 16:43:55 -0700

    speedtest fixed

--- install/optional/app-speedtest.sh
+++ install/optional/app-speedtest.sh
@@ -3,25 +3,30 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../../common_functions.sh"
 
-echo "Checking for curl installation..."
-if ! command -v curl &> /dev/null; then
-    echo "curl is not installed. Installing curl..."
-    sudo apt update
-    sudo apt install -y curl
+
+if command_exists speedtest-cli; then
+    print_success "Speedtest CLI is already installed. Exiting script."
+else
+    echo "Checking for curl installation..."
+    if ! command_exists curl &> /dev/null; then
+        echo "curl is not installed. Installing curl..."
+        sudo apt update
+        sudo apt install -y curl
+        if [ $? -ne 0 ]; then
+            print_error "Failed to install curl. Exiting."
+            exit 1
+        fi
+        print_success "curl installed successfully."
+    else
+        print_success "curl is already installed."
+    fi
+
+    echo "Installing Speedtest CLI..."
+    curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
     if [ $? -ne 0 ]; then
-        print_error "Failed to install curl. Exiting."
+        print_error "Failed to add Speedtest CLI repository. Exiting."
         exit 1
     fi
-    print_success "curl installed successfully."
-else
-    print_success "curl is already installed."
-fi
 
-echo "Installing Speedtest CLI..."
-curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-if [ $? -ne 0 ]; then
-    print_error "Failed to add Speedtest CLI repository. Exiting."
-    exit 1
+    apt_install speedtest-cli
 fi
-
-apt_install speedtest-cli
