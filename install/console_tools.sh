33e6195a84f2e69088dc046601427731f236a646
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-08-15 09:44:14 -0700

    consle tools added

--- install/console_tools.sh
+++ install/console_tools.sh
@@ -4,3 +4,15 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../common_functions.sh"
 
 apt_install screen
+
+apt_install htop
+
+apt_install btop
+
+apt_install eza
+
+apt_install fd-find
+
+apt_install plocate
+
+apt_install zoxide
