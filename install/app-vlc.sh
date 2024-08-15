980e1ab6e9eb8fa68b3170a31036a902200ca0e2
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-08-15 09:29:37 -0700

    installation cleaned

--- install/app-vlc.sh
+++ install/app-vlc.sh
@@ -3,11 +3,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../common_functions.sh"
 
-if command_exists vlc; then
-    print_success "VLC is already installed. Exiting script."
-else
-	apt_install vlc
-
-fi
-
-print_success "VLC installed successfully."
+apt_install vlc
