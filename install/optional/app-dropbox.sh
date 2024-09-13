62c57b5b3c5bd55164b54fcc855ae78b3c90e26b
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-09-13 15:05:58 +0200

    fixed dropbox

--- install/optional/app-dropbox.sh
+++ install/optional/app-dropbox.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../../common_functions.sh"
 
-install_package "Dropbox" "nauilus-dropbox" "nauilus-dropbox" "apt" "None"
+install_package "Dropbox" "nautilus-dropbox" "nautilus-dropbox" "apt" "None"
