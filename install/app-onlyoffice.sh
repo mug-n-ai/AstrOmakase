215e69fda05264abd1d4f516b76c8d6dc2bb339c
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-10-09 20:00:12 +0200

    onlyoffice moved to snap

--- install/app-onlyoffice.sh
+++ install/app-onlyoffice.sh
@@ -3,18 +3,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../common_functions.sh"
 
-if command_exists onlyoffice-desktopeditors; then
-
-    print_success "OnlyOffice is already installed. Exiting script."
-
-else
-    echo "adding repository..."
-    mkdir -p -m 700 ~/.gnupg
-    gpg --no-default-keyring --keyring gnupg-ring:/tmp/onlyoffice.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
-    chmod 644 /tmp/onlyoffice.gpg
-    sudo chown root:root /tmp/onlyoffice.gpg
-    sudo mv /tmp/onlyoffice.gpg /usr/share/keyrings/onlyoffice.gpg
-    echo 'deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
-
-    apt_install onlyoffice-desktopeditors
-fi
+install_package OnlyOffice onlyoffice-desktopeditors onlyoffice-desktopeditors onlyoffice-desktopeditors snap
