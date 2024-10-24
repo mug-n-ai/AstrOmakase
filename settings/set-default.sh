67d215dce75554b0bb65b70ac85e7447e80d81e5
Lorenzo Mugnai <lorenzomugnai09@gmail.com>
2024-10-24 17:39:00 +0200

    updated zed compatibilities

--- settings/set-default.sh
+++ settings/set-default.sh
@@ -16,7 +16,6 @@ xdg-mime default zed.desktop text/plain
 xdg-mime default zed.desktop text/x-python
 xdg-mime default zed.desktop application/octet-stream
 xdg-mime default zed.desktop text/x-shellscript
-xdg-mime default zed.desktop application/x-shellscript
 xdg-mime default zed.desktop text/x-log
 xdg-mime default zed.desktop text/xml
 xdg-mime default zed.desktop application/xml
@@ -27,6 +26,11 @@ xdg-mime default zed.desktop text/x-chdr
 xdg-mime default zed.desktop text/x-c++
 xdg-mime default zed.desktop text/x-c++hdr
 xdg-mime default zed.desktop text/x-c++src
+xdg-mime default zed.desktop text/markdown
+xdg-mime default zed.desktop application/json
+xdg-mime default zed.desktop text/x-ini
+xdg-mime default zed.desktop text/x-config
+
 
 # set Okular as default for PDF files
 xdg-mime default okular_okular.desktop application/pdf
