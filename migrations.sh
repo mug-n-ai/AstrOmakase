54cf6acca0f2ef64d89b7ee491705c10f7518ba9
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-11-29 11:37:12 +0100

    change speedtest repos

--- migrations.sh
+++ migrations.sh
@@ -15,6 +15,14 @@ if [ -f ~/.local/share/astromakase/version_previous ]; then
             sudo apt autoremove -y
             echo "Migration complete."
         fi
+    if [ "$PREVIOUS_VERSION" == "0.2.5" ]; then
+        if dpkg -l | grep -q okular; then
+            echo "Migrating changing repository for speedtest..."
+            sudo apt remove speedtest-cli -y
+            sudo rm /etc/apt/sources.list.d/ookla_speedtest-cli.list
+            sudo rm /etc/apt/keyrings/ookla_speedtest-cli-archive-keyring.gpg
+            echo "Migration complete."
+        fi
     fi 
 
     rm ~/.local/share/astromakase/version_previous
