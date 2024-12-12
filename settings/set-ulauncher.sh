30c8d2ae3adbedae71c1f2c60e69ab747ab8b49b
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-12-12 06:37:45 +0100

    tentative fix for ulauncher settngs

--- settings/set-ulauncher.sh
+++ settings/set-ulauncher.sh
@@ -1,8 +1,8 @@
 # This settings are inspired by the Omakub project. Please visit the [Omakub project](https://github.com/basecamp/omakub) 
 
 # Start ulauncher to have it populate config before we overwrite
-mkdir -p ~/.config/autostart/
-cp ~/.local/share/astromakase/settings/config/ulauncher/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
+mkdir -p $HOME/.config/autostart/
+cp $HOME/.local/share/astromakase/settings/config/ulauncher/ulauncher.desktop $HOME/.config/autostart/ulauncher.desktop
 gtk-launch ulauncher.desktop >/dev/null 2>&1
 sleep 2 # ensure enough time for ulauncher to set defaults
-cp ~/.local/share/astromakase/settings/config/ulauncher/ulauncher.json ~/.config/ulauncher/settings.json
+cp $HOME/.local/share/astromakase/settings/config/ulauncher/ulauncher.json $HOME/.config/ulauncher/settings.json
