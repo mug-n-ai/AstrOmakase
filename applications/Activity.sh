7641a3be8e6f7414f8452e3f5630811edcb880dd
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-12-12 17:56:44 +0100

    Activity launcher updated

--- applications/Activity.sh
+++ applications/Activity.sh
@@ -3,7 +3,7 @@ cat <<EOF >~/.local/share/applications/Activity.desktop
 Version=1.0
 Name=Activity
 Comment=System activity from btop
-Exec=gnome-terminal --geometry=120x80 -- btop
+Exec=gnome-terminal --geometry=120x30 -- btop
 Terminal=false
 Type=Application
 Icon=/home/$USER/.local/share/astromakase/applications/icons/Activity.png
