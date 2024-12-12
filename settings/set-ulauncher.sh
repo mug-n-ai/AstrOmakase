ec106ecddc1edc88e39ac91d143b9745cbd798d6
LorenzoMugnai <lorenzomugnai09@gmail.com>
2024-12-12 06:23:30 +0100

    fixing ulauncher settings

--- settings/set-ulauncher.sh
+++ settings/set-ulauncher.sh
@@ -2,7 +2,7 @@
 
 # Start ulauncher to have it populate config before we overwrite
 mkdir -p ~/.config/autostart/
-cp ~/.local/share/astromakase/config/ulauncher/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
+cp ~/.local/share/astromakase/settings/config/ulauncher/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
 gtk-launch ulauncher.desktop >/dev/null 2>&1
 sleep 2 # ensure enough time for ulauncher to set defaults
-cp ~/.local/share/astromakase/config/ulauncher/ulauncher.json ~/.config/ulauncher/settings.json
+cp ~/.local/share/astromakase/settings/config/ulauncher/ulauncher.json ~/.config/ulauncher/settings.json
